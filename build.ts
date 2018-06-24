2a846d01ea22ee7cbe89ae4f857548c2dc6d27f4
Scott Cooper <scttcper@gmail.com>
2018-06-24 12:25:07 -0700

    fix: publish as commonjs and module using typescript, remove rollup

--- build.ts
+++ build.ts
@@ -1,30 +1,14 @@
 import { join } from 'path';
 
 import { copySync } from 'fs-extra';
-import { rollup, OutputOptions, RollupFileOptions } from 'rollup';
-import * as sourceMaps from 'rollup-plugin-sourcemaps';
-
-const moduleInputOptions: RollupFileOptions = {
-  input: `dist/esm5/index.js`,
-  plugins: [sourceMaps()],
-};
-const moduleOutputOptions: OutputOptions = {
-  file: './dist/package-dist/bundles/gaussian.es2015.js',
-  format: 'es',
-  sourcemap: true,
-};
 
 async function build() {
-  // create bundles
-  const mod = await rollup(moduleInputOptions);
-  await mod.write(moduleOutputOptions);
-
   // copy git folder to dist folder for semantic-release
-  copySync('.git', join(process.cwd(), 'dist/package-dist/.git'));
+  copySync('.git', join(process.cwd(), 'dist/.git'));
   // copy files to distribution folder
-  copySync('package.json', join(process.cwd(), 'dist/package-dist/package.json'));
-  copySync('README.md', join(process.cwd(), 'dist/package-dist/README.md'));
-  copySync('LICENSE', join(process.cwd(), 'dist/package-dist/LICENSE'));
+  copySync('package.json', join(process.cwd(), 'dist/package.json'));
+  copySync('README.md', join(process.cwd(), 'dist/README.md'));
+  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 build()
