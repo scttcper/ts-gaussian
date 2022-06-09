1faba7a9d064b9db8a02c6f04c88e6d4895319ae
Scott Cooper <scttcper@gmail.com>
2022-06-08 23:26:14 -0700

    fix: upgrade dependencies

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,101 +1,101 @@
 // Tests based on values from Wolfram Alpha.
-import test, { ExecutionContext } from 'ava';
+import { describe, expect, it } from 'vitest';
 
 import { Gaussian } from '../src/index.js';
 
-function epsilonEqual(t: ExecutionContext, actual: number, expected: number) {
+function epsilonEqual(actual: number, expected: number): void {
   const diff = Math.abs(actual - expected);
-  t.truthy(diff < 1e-5);
+  expect(diff < 1e-5).toBeTruthy();
 }
 
-function gaussianEqual(t: ExecutionContext, actual: Gaussian, expected: Gaussian) {
-  t.is(actual.mean, expected.mean);
-  t.is(actual.variance, expected.variance);
+function gaussianEqual(actual: Gaussian, expected: Gaussian): void {
+  expect(actual.mean).toEqual(expected.mean);
+  expect(actual.variance).toEqual(expected.variance);
 }
 
-test('should have default properties', t => {
+it('should have default properties', () => {
   const d1 = new Gaussian(0, 1);
-  t.is(d1.mean, 0);
-  t.is(d1.variance, 1);
-  t.is(d1.standardDeviation, 1);
+  expect(d1.mean).toEqual(0);
+  expect(d1.variance).toEqual(1);
+  expect(d1.standardDeviation).toEqual(1);
 
   const d2 = new Gaussian(1, 4);
-  t.is(d2.mean, 1);
-  t.is(d2.variance, 4);
-  t.is(d2.standardDeviation, 2);
+  expect(d2.mean).toEqual(1);
+  expect(d2.variance).toEqual(4);
+  expect(d2.standardDeviation).toEqual(2);
 });
 
-test('should compute pdf', t => {
+it('should compute pdf', () => {
   const d = new Gaussian(0, 1);
-  epsilonEqual(t, d.pdf(-2), 0.053991);
-  epsilonEqual(t, d.pdf(-1), 0.241971);
-  epsilonEqual(t, d.pdf(0), 0.398942);
-  epsilonEqual(t, d.pdf(1), 0.241971);
-  epsilonEqual(t, d.pdf(2), 0.053991);
+  epsilonEqual(d.pdf(-2), 0.053991);
+  epsilonEqual(d.pdf(-1), 0.241971);
+  epsilonEqual(d.pdf(0), 0.398942);
+  epsilonEqual(d.pdf(1), 0.241971);
+  epsilonEqual(d.pdf(2), 0.053991);
 });
 
-test('should compute cdf', t => {
+it('should compute cdf', () => {
   const d = new Gaussian(0, 1);
-  epsilonEqual(t, d.cdf(-1.28155), 0.1);
-  epsilonEqual(t, d.cdf(-0.67449), 0.25);
-  epsilonEqual(t, d.cdf(0), 0.5);
-  epsilonEqual(t, d.cdf(0.67449), 0.75);
-  epsilonEqual(t, d.cdf(1.28155), 0.9);
+  epsilonEqual(d.cdf(-1.28155), 0.1);
+  epsilonEqual(d.cdf(-0.67449), 0.25);
+  epsilonEqual(d.cdf(0), 0.5);
+  epsilonEqual(d.cdf(0.67449), 0.75);
+  epsilonEqual(d.cdf(1.28155), 0.9);
 });
 
-test('should compute ppf', t => {
+it('should compute ppf', () => {
   const d = new Gaussian(0, 1);
-  epsilonEqual(t, d.ppf(0.1), -1.28155);
-  epsilonEqual(t, d.ppf(0.25), -0.67449);
-  epsilonEqual(t, d.ppf(0.5), 0);
-  epsilonEqual(t, d.ppf(0.75), 0.67449);
-  epsilonEqual(t, d.ppf(0.9), 1.28155);
+  epsilonEqual(d.ppf(0.1), -1.28155);
+  epsilonEqual(d.ppf(0.25), -0.67449);
+  epsilonEqual(d.ppf(0.5), 0);
+  epsilonEqual(d.ppf(0.75), 0.67449);
+  epsilonEqual(d.ppf(0.9), 1.28155);
 });
 
-test('should compute mul', t => {
+it('should compute mul', () => {
   // Test normal mul.
   const d = new Gaussian(0, 1).mul(new Gaussian(0, 1));
-  gaussianEqual(t, d, new Gaussian(0, 0.5));
+  gaussianEqual(d, new Gaussian(0, 0.5));
   // Test scale.
-  gaussianEqual(t, new Gaussian(1, 1).mul(2), new Gaussian(2, 4));
+  gaussianEqual(new Gaussian(1, 1).mul(2), new Gaussian(2, 4));
 });
 
-test('should compute div', t => {
+it('should compute div', () => {
   // Test normal div.
   const d = new Gaussian(1, 1).div(new Gaussian(1, 2));
-  gaussianEqual(t, d, new Gaussian(1, 2));
+  gaussianEqual(d, new Gaussian(1, 2));
   // Test scale.
-  gaussianEqual(t, new Gaussian(1, 1).div(1 / 2), new Gaussian(2, 4));
+  gaussianEqual(new Gaussian(1, 1).div(1 / 2), new Gaussian(2, 4));
 });
 
-test('should compute add', t => {
-  gaussianEqual(t, new Gaussian(1, 1).add(new Gaussian(1, 2)), new Gaussian(2, 3));
+it('should compute add', () => {
+  gaussianEqual(new Gaussian(1, 1).add(new Gaussian(1, 2)), new Gaussian(2, 3));
 });
 
-test('should compute sub', t => {
-  gaussianEqual(t, new Gaussian(1, 1).sub(new Gaussian(1, 2)), new Gaussian(0, 3));
+it('should compute sub', () => {
+  gaussianEqual(new Gaussian(1, 1).sub(new Gaussian(1, 2)), new Gaussian(0, 3));
 });
 
-test('should compute scale', t => {
-  gaussianEqual(t, new Gaussian(1, 1).scale(2), new Gaussian(2, 4));
+it('should compute scale', () => {
+  gaussianEqual(new Gaussian(1, 1).scale(2), new Gaussian(2, 4));
 });
 
-test('should rejects non-positive variances', t => {
-  t.throws(() => new Gaussian(0, 0));
-  t.throws(() => new Gaussian(0, -1));
+it('should rejects non-positive variances', () => {
+  expect(() => new Gaussian(0, 0)).toThrowError();
+  expect(() => new Gaussian(0, -1)).toThrowError();
 });
 
-test('ceils ppf >= 1', t => {
+it('ceils ppf >= 1', () => {
   const normal = new Gaussian(0, 1);
-  t.is(normal.ppf(1), 141.4213562373095);
-  t.is(normal.ppf(1.1), 141.4213562373095);
-  t.is(normal.ppf(100), 141.4213562373095);
+  expect(normal.ppf(1)).toBe(141.4213562373095);
+  expect(normal.ppf(1.1)).toBe(141.4213562373095);
+  expect(normal.ppf(100)).toBe(141.4213562373095);
 });
 
-test('ceils ppf <= 0', t => {
+it('ceils ppf <= 0', () => {
   const normal = new Gaussian(0, 1);
-  t.is(normal.ppf(0), -141.4213562373095);
-  t.is(normal.ppf(-0), -141.4213562373095);
-  t.is(normal.ppf(-0.1), -141.4213562373095);
-  t.is(normal.ppf(-1), -141.4213562373095);
+  expect(normal.ppf(0)).toBe(-141.4213562373095);
+  expect(normal.ppf(-0)).toBe(-141.4213562373095);
+  expect(normal.ppf(-0.1)).toBe(-141.4213562373095);
+  expect(normal.ppf(-1)).toBe(-141.4213562373095);
 });
